ff98945676bf6f404f2b04335f06a20bee25f21c
SahilMaz27 <sahil.mazumder@buyeazzy.com>
2026-07-15 11:06:29 +0530

    analytics added

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const geist = Geist({
   subsets: ["latin"],
@@ -13,7 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={geist.className}>{children}</body>
+      <body className={geist.className}>
+        {children}
+
+        <Analytics />
+      </body>
     </html>
   );
 }
